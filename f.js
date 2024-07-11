06aa1463122390c34af3ad8013496375ef5bea17
Fidan_Alisoy <alisoyfidan11@gmail.com>
2024-07-11 13:21:59 +0400

    Add files via upload

---
+++ f.js
@@ -0,0 +1,27 @@
+var colors = [ "red", "blue", "green", "yellow", "orange", "purple", "brown", "black"]
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+var shuffled_colors = shuffleArray(colors);
+console.log(shuffled_colors);
+
+// var cards =""
+// for(var index=0; index < shuffled_colors.length; index++)
+//     cards += '<div class="card"></div>'
+
+  const cards = document.querySelectorAll(".card");
+  
+  function flipCard() {
+    this.classList.add("flipped");
+  }
+  
+  
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", flipCard);
+  }
